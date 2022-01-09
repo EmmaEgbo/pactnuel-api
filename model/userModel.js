c7d6f52adff0fe5cd69d45c3b36a78a8f164c440
Rasheed Ayoade <rashotech@gmail.com>
2022-01-09 13:44:55 +0100

    Token life extended

--- model/userModel.js
+++ model/userModel.js
@@ -168,10 +168,10 @@ const saveToken = async (token, userId, expires, blacklisted = false) => {
 };
 
 exports.generateAuthTokens = async (userData) => {
-  const accessTokenExpires = moment().add(15, 'minutes');
+  const accessTokenExpires = moment().add(30, 'minutes');
   const accessToken = await generateToken(userData, accessTokenExpires);
 
-  const refreshTokenExpires = moment().add(7, 'days');
+  const refreshTokenExpires = moment().add(14, 'days');
   const refreshToken = await generateToken(userData, refreshTokenExpires, true);
   await saveToken(refreshToken, userData.ID, refreshTokenExpires);
 
